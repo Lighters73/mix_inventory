ae6159acf84792a6b85a8c1d4eff879a98bf99be
Steven J Lightfoot <steven.j.lightfoot@outlook.com>
2022-12-22 21:09:14 +0000

    added: move stock to output zone, get last location on stock move

--- mix_inventory/mix_inventory/doctype/stock_customer_shipment/stock_customer_shipment.js
+++ mix_inventory/mix_inventory/doctype/stock_customer_shipment/stock_customer_shipment.js
@@ -20,7 +20,12 @@ frappe.ui.form.on('Stock Customer Shipment', {
 frappe.ui.form.on('Stock Move', {
 	stock_move_add(frm, cdt, cdn) {
 		// set default value for destination
-		frappe.model.set_value(cdt, cdn, 'destination_location', 'LOC-CUS')
+		frappe.model.set_value(cdt, cdn, 'destination_location', 'LOC_CUS')
+		// set the default source_location to be the source location of the previous item
+		if (frm.doc.stock_move.length > 1) {
+			let last_source_location = frm.doc.stock_move[frm.doc.stock_move.length - 2].source_location
+			frappe.model.set_value(cdt, cdn, 'source_location', last_source_location)
+		}
 	}
 });
 
