4b70b09664c4811f10afd30c5a29b0f326e941fe
Steven J Lightfoot <steven.j.lightfoot@outlook.com>
2023-01-06 16:50:13 +0000

    remove: output zone

--- mix_inventory/mix_inventory/doctype/stock_customer_shipment/stock_customer_shipment.py
+++ mix_inventory/mix_inventory/doctype/stock_customer_shipment/stock_customer_shipment.py
@@ -7,45 +7,22 @@ from frappe.model.document import Document
 
 
 class StockCustomerShipment(Document):
-    output_zone = 'LOC_OUT'
 
     def before_save(self):
-        output_key: str
-        source_key: str
+        # update the items to draft
         for move in self.stock_move:
-            # move the stock to the output zone
-            source_key = f'{move.source_location}-{move.product}'
-            output_key = f'{self.output_zone}-{move.product}'
-            # validate source item
-            self.validate_item(source_key)
-            # create a destination record if it does not exist
-            if not frappe.db.exists('Stock Quantity', output_key):
-                doc = frappe.get_doc({
-                    'doctype': 'Stock Quantity',
-                    'location': self.output_zone,
-                    'product': move.product,
-                    'quantity': move.qty
-                })
-                doc.insert()
             move.state = 'Draft'
-            # update the quantities - decrease from source location, increase output_zone
-            sq_source_doc = frappe.get_doc('Stock Quantity', source_key)
-            sq_source_doc.qty = sq_source_doc.qty - move.qty
-            sq_output_doc = frappe.get_doc('Stock Quantity', output_key)
-            sq_output_doc.qty = sq_output_doc.qty + move.qty
-            sq_source_doc.save()
-            sq_output_doc.save()
 
     def before_submit(self):
         # update the quantities
-        output_key: str
+        source_key: str
         destination_key: str
         for move in self.stock_move:
-            output_key = f'{self.output_zone}-{move.product}'
+            source_key = f'{move.source_location}-{move.product}'
             destination_key = f'{move.destination_location}-{move.product}'
-            # validate output item
-            self.validate_item(output_key)
-            # create a stock quantity record if it does not exist
+            # validate item from source_location
+            self.validate_item(source_key)
+            # create a stock quantity record for the destination item if it does not exist
             if not frappe.db.exists('Stock Quantity', destination_key):
                 doc = frappe.get_doc({
                     'doctype': 'Stock Quantity',
@@ -55,30 +32,32 @@ class StockCustomerShipment(Document):
                 })
                 doc.insert()
             move.state = 'Submitted'
-            # update the quantities - decrease from output zone, increase destination location
-            sq_output_doc = frappe.get_doc('Stock Quantity', output_key)
-            sq_output_doc.qty = sq_output_doc.qty - move.qty
+            # update the quantities - decrease from source location, increase destination location
+            sq_source_doc = frappe.get_doc('Stock Quantity', source_key)
+            sq_source_doc.qty = sq_source_doc.qty - move.qty
             sq_destination_doc = frappe.get_doc('Stock Quantity', destination_key)
             sq_destination_doc.qty = sq_destination_doc.qty + move.qty
-            sq_output_doc.save()
+            sq_source_doc.save()
             sq_destination_doc.save()
 
     def validate_item(self, lookup_key):
-		# from the lookup key - extract the product and location
+        # from the lookup key - extract the product and location
         keys = str(lookup_key).split('-')
-		# check the item exists at the location - you can't move stock from a location that does not exist
+        # check the item exists at the location - you can't move stock from a location that does not exist
         if not frappe.db.exists('Stock Quantity', lookup_key):
             frappe.throw(_("Can't proceed, check you have this item at this location\n"
                            "Product: {0} Location: {1}\n".format(keys[1], keys[0])))
 
     def before_cancel(self):
-        # check what the state of the document is
+        # reverse the stock move quantities
         for move in self.stock_move:
-            if move.state == 'Draft':  # reverse move to output zone
-                lookup_key = f'{self.output_zone}-{move.product}'
-            else:  # reverse move to destination
-                lookup_key = f'{move.destination_location}-{move.product}'
+            source_key = f'{move.source_location}-{move.product}'
+            destination_key = f'{move.destination_location}-{move.product}'
             move.state = 'Cancelled'
-            stock_quantity_doc = frappe.get_doc('Stock Quantity', lookup_key)
-            stock_quantity_doc.qty = stock_quantity_doc.qty - move.qty
-            stock_quantity_doc.save()
+            # update the quantities - decrease from destination location, increase source location
+            sq_destination_doc = frappe.get_doc('Stock Quantity', destination_key)
+            sq_destination_doc.qty = sq_destination_doc.qty - move.qty
+            sq_source_doc = frappe.get_doc('Stock Quantity', source_key)
+            sq_source_doc.qty = sq_source_doc.qty + move.qty
+            sq_destination_doc.save()
+            sq_source_doc.save()
